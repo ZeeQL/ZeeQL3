f980c658c44193c33f2d5cc345892b2d5ed628a1
Helge Hess <devteam@zeezide.de>
2017-09-29 16:21:00 +0200

    Update shims to point to Xcode 9 release
    If it is older, we assume /Applications/Xcode 8.3.app/ as the
    place to be.

--- CSQLite3/watchos/CSQLite3Shim.h
+++ CSQLite3/watchos/CSQLite3Shim.h
@@ -2,10 +2,10 @@
 #define __CSQLITE3_SHIM_H__
 
 #if defined(__APPLE__) && defined(__MACH__)
-#  if __clang_major__ >= 9 // until the release ...
-#    include "/Applications/Xcode-beta.app/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk/usr/include/sqlite3.h"
-#  else
+#  if __clang_major__ >= 9 // released now. assume Xcode 9.
 #    include "/Applications/Xcode.app/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk/usr/include/sqlite3.h"
+#  else
+#    include "/Applications/Xcode 8.3.app/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk/usr/include/sqlite3.h"
 #  endif
 #else
 #  include "/usr/include/sqlite3.h"
