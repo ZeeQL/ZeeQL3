ae99256b6fb7f0ef5b5289d9114962d712436b3d
Helge Hess <devteam@zeezide.de>
2017-09-29 16:40:09 +0200

    Update shims to point to Xcode 9 release
    ... forgot the plain macos one.

--- CSQLite3/macos/CSQLite3Shim.h
+++ CSQLite3/macos/CSQLite3Shim.h
@@ -2,8 +2,8 @@
 #define __CSQLITE3_SHIM_H__
 
 #if defined(__APPLE__) && defined(__MACH__)
-#  if __clang_major__ >= 9 // until the release ...
-#    include "/Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sqlite3.h"
+#  if __clang_major__ >= 9 // released now. assume Xcode 9.
+#    include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sqlite3.h"
 #  else
 #    include "/usr/include/sqlite3.h"
 #  endif
