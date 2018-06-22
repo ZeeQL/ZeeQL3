029784ffc6c3785e1a54533a89abb96538d5ccc1
Helge Hess <devteam@zeezide.de>
2018-06-22 11:04:38 +0200

    XcodeXbeta: Adjust shims to point to 2nd Xcode
    Modulemaps require absolute pathes. For XcodeXbeta
    (clang 10) we point them to /Applications/Xcode-beta.app.
    
    This needs to be reverted once XcodeX is released.

--- CSQLite3/macos/CSQLite3Shim.h
+++ CSQLite3/macos/CSQLite3Shim.h
@@ -2,7 +2,9 @@
 #define __CSQLITE3_SHIM_H__
 
 #if defined(__APPLE__) && defined(__MACH__)
-#  if __clang_major__ >= 9 // released now. assume Xcode 9.
+#  if __clang_major__ >= 10 // Assume Xcode-beta.app (XcodeXbeta)
+#    include "/Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sqlite3.h"
+#  elif __clang_major__ >= 9
 #    include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sqlite3.h"
 #  else
 #    include "/usr/include/sqlite3.h"
--- CSQLite3/watchos-simulator/CSQLite3Shim.h
+++ CSQLite3/watchos-simulator/CSQLite3Shim.h
@@ -2,10 +2,10 @@
 #define __CSQLITE3_SHIM_H__
 
 #if defined(__APPLE__) && defined(__MACH__)
-#  if __clang_major__ >= 9 // released now. assume Xcode 9.
-#    include "/Applications/Xcode.app/Contents/Developer/Platforms/WatchSimulator.platform/Developer/SDKs/WatchSimulator.sdk/usr/include/sqlite3.h"
+#  if __clang_major__ >= 10 // Assume Xcode-beta.app (XcodeXbeta)
+#    include "/Applications/Xcode-beta.app/Contents/Developer/Platforms/WatchSimulator.platform/Developer/SDKs/WatchSimulator.sdk/usr/include/sqlite3.h"
 #  else
-#    include "/Applications/Xcode 8.3.app/Contents/Developer/Platforms/WatchSimulator.platform/Developer/SDKs/WatchSimulator.sdk/usr/include/sqlite3.h"
+#    include "/Applications/Xcode.app/Contents/Developer/Platforms/WatchSimulator.platform/Developer/SDKs/WatchSimulator.sdk/usr/include/sqlite3.h"
 #  endif
 #else
 #  include "/usr/include/sqlite3.h"
