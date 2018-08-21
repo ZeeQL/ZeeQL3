6c673e3a7d9d9fd8c39c283f359475d6dbca63c9
Helge Heß <devteam@zeezide.de>
2018-08-21 18:23:00 +0200

    Add Swift 4 manifest
    ... sigh.

---
+++ Package@swift-4.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+  name: "ZeeQL3",
+
+  products: [ // TBD: Use ZeeQL3 as library name?
+    .library(name: "ZeeQL", targets: [ "ZeeQL" ])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ZeeQL/CSQLite3.git",
+             from: "1.0.0")
+  ],
+  targets: [
+    .target(name: "ZeeQL")
+  ]
+)
