bc53c2793806c4ca14df6c8a60c8128ac95b4a70
Helge Heß <devteam@zeezide.de>
2019-04-10 18:57:00 +0200

    Prep Swift 5 package build
    ...

---
+++ Package@swift-5.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+  name: "ZeeQL3",
+
+  products: [ // TBD: Use ZeeQL3 as library name?
+    .library(name: "ZeeQL", targets: [ "ZeeQL" ])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ZeeQL/CSQLite3.git",
+             from: "2.0.0")
+  ],
+  targets: [
+    .target(name: "ZeeQL"),
+    .testTarget(name: "ZeeQLTests", dependencies: [ "ZeeQL" ])
+  ]
+)
