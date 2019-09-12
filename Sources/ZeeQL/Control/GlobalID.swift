1ac731aea2105a8dd8785fa18b78a10611c8c7ad
Helge Heß <devteam@zeezide.de>
2019-09-12 15:36:43 +0200

    Add description to GIDs, improve int-gids
    Create int GIDs for more int types.

--- Sources/ZeeQL/Control/GlobalID.swift
+++ Sources/ZeeQL/Control/GlobalID.swift
@@ -16,10 +16,12 @@ open class GlobalID : EquatableType, Hashable {
     return gid === self
   }
   
+  @inlinable
   open func hash(into hasher: inout Hasher) {
     // well, what?? :-)
   }
   
+  @inlinable
   public static func ==(lhs: GlobalID, rhs: GlobalID) -> Bool {
     return lhs.isEqual(to: rhs)
   }
@@ -34,23 +36,47 @@ open class GlobalID : EquatableType, Hashable {
  * Note: this is an abstract class, when building new keys using
  * globalIDWithEntityName(), you will get private subclasses.
  */
-public class KeyGlobalID : GlobalID {
+public class KeyGlobalID : GlobalID, CustomStringConvertible {
   
   public let entityName : String
   
+  @inlinable
   public init(entityName: String) {
     self.entityName = entityName
   }
   
+  @inlinable
   open var keyCount : Int { return 0 }
   
+  @inlinable
   open subscript(i: Int) -> Any? { return nil }
 
   static func make(entityName: String, values: [ Any? ]) -> KeyGlobalID {
-    if values.count == 1, let i = values[0] as? Int {
-      return SingleIntKeyGlobalID(entityName: entityName, value: i)
+    if values.count == 1, let v = values.first {
+      switch v {
+        case let i as Int:
+          return SingleIntKeyGlobalID(entityName: entityName, value: i)
+        case let i as Int64:
+          return SingleIntKeyGlobalID(entityName: entityName, value: Int(i))
+        case let i as Int32:
+          return SingleIntKeyGlobalID(entityName: entityName, value: Int(i))
+        case let i as UInt32: // assumes 64-bit
+          return SingleIntKeyGlobalID(entityName: entityName, value: Int(i))
+        default:
+          break
+      }
     }
     return ComplexKeyGlobalID(entityName: entityName, values: values)
+  }
+
+  public var description: String {
+    var ms = "<GID: \(entityName)"
+    for i in 0..<keyCount {
+      if let v = self[i] { ms += " \(v)"  }
+      else               { ms += " <nil>" }
+    }
+    ms += ">"
+    return ms
   }
 }
 
@@ -65,18 +91,22 @@ public final class ComplexKeyGlobalID : KeyGlobalID {
     super.init(entityName: entityName)
   }
   
+  @inlinable
   override public var keyCount : Int { return values.count }
   
+  @inlinable
   override public subscript(i: Int) -> Any? {
     guard i < values.count else { return nil }
     return values[i]
   }
   
+  @inlinable
   override public func hash(into hasher: inout Hasher) {
     entityName.hash(into: &hasher)
     String(describing: values[0]).hash(into: &hasher)
   }
   
+  @inlinable
   override public func isEqual(to object: Any?) -> Bool {
     // TODO: compare against SingleIntKeyGlobalID
     guard let gid = object as? ComplexKeyGlobalID else { return false }
@@ -94,26 +124,40 @@ public final class SingleIntKeyGlobalID : KeyGlobalID {
   
   public let value : Int
   
+  @inlinable
   public init(entityName: String, value: Int) {
     self.value = value
     super.init(entityName: entityName)
   }
 
+  @inlinable
   override public var keyCount : Int { return 1 }
 
+  @inlinable
   override public subscript(i: Int) -> Any? {
     guard i == 0 else { return nil }
     return value
   }
   
+  @inlinable
   override public func hash(into hasher: inout Hasher) {
     entityName.hash(into: &hasher)
     value     .hash(into: &hasher)
   }
   
+  @inlinable
   override public func isEqual(to object: Any?) -> Bool {
-    // TODO: compare against ComplexKeyGlobalID
-    guard let gid = object as? SingleIntKeyGlobalID else { return false }
+    guard let gid = object as? SingleIntKeyGlobalID else {
+      if let complex = object as? ComplexKeyGlobalID {
+        return complex.isEqual(to: self)
+      }
+      return false
+    }
     return value == gid.value && entityName == gid.entityName
   }
+
+  override public var description: String {
+    return "<GIDi: \(entityName) \(value)>"
+  }
 }
+
