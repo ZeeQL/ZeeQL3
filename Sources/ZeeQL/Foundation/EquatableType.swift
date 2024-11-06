1ddf64a8bae4d3a0c70da5d3a19a5154d5ad8da4
Helge Heß <devteam@zeezide.de>
2024-11-06 13:29:04 +0100

    Support Equatable/Comparable in Swift 5.5+
    Using existential box unwrapping.

--- Sources/ZeeQL/Foundation/EquatableType.swift
+++ Sources/ZeeQL/Foundation/EquatableType.swift
@@ -3,7 +3,7 @@
 //  ZeeQL
 //
 //  Created by Helge Heß on 17.02.17.
-//  Copyright © 2017-2019 ZeeZide GmbH. All rights reserved.
+//  Copyright © 2017-2024 ZeeZide GmbH. All rights reserved.
 //
 
 /**
@@ -34,6 +34,7 @@ public protocol LikeComparisonType {
 }
 
 
+@inlinable
 public func eq<T: Equatable>(_ a: T?, _ b: T?) -> Bool {
   if let a = a, let b = b {
     return a == b
@@ -42,6 +43,7 @@ public func eq<T: Equatable>(_ a: T?, _ b: T?) -> Bool {
     return a == nil && b == nil
   }
 }
+@inlinable
 public func isSmaller<T: Comparable>(_ a: T?, _ b: T?) -> Bool {
   if let a = a, let b = b {
     return a < b
@@ -54,18 +56,44 @@ public func isSmaller<T: Comparable>(_ a: T?, _ b: T?) -> Bool {
   }
 }
 
+@inlinable
 public func eq(_ a: Any?, _ b: Any?) -> Bool {
   if let a = a, let b = b {
-    guard let a = a as? EquatableType else { return false }
+    guard let a = a as? EquatableType else {
+      #if swift(>=5.5)
+      // Unwrapping dance
+      func _isEqual<T: Equatable>(lhs: T, rhs: Any) -> Bool {
+        guard let rhs = rhs as? T else { return false }
+        return lhs == rhs
+      }
+      guard let lhs = a as? any Equatable else { return false }
+      return _isEqual(lhs: lhs, rhs: b)
+      #else
+      return false
+      #endif
+    }
     return a.isEqual(to: b)
   }
   else {
     return a == nil && b == nil
   }
 }
+@inlinable
 public func isSmaller(_ a: Any?, _ b: Any?) -> Bool {
   if let a = a, let b = b {
-    guard let a = a as? ComparableType else { return false }
+    guard let a = a as? ComparableType else {
+      #if swift(>=5.5)
+      // Unwrapping dance
+      func _isSmaller<T: Comparable>(lhs: T, rhs: Any) -> Bool {
+        guard let rhs = rhs as? T else { return false }
+        return lhs < rhs
+      }
+      guard let lhs = a as? any Comparable else { return false }
+      return _isSmaller(lhs: lhs, rhs: b)
+      #else
+      return false
+      #endif
+    }
     return a.isSmaller(than: b)
   }
   else if a == nil && b == nil {
@@ -82,6 +110,8 @@ public func isSmaller(_ a: Any?, _ b: Any?) -> Bool {
 // TBD: is there a betta way? :-)
 
 public extension FixedWidthInteger {
+  
+  @inlinable
   func isEqual(to object: Any?) -> Bool {
     guard let object = object else { return false } // other is nil
     switch object {
@@ -99,6 +129,8 @@ public extension FixedWidthInteger {
       default: return false
     }
   }
+  
+  @inlinable
   func isSmaller(than object: Any?) -> Bool {
     guard let object = object else { return false } // other is nil
     switch object {
@@ -128,20 +160,24 @@ extension UInt32 : EquatableType, ComparableType {}
 extension UInt64 : EquatableType, ComparableType {}
 
 extension Float : EquatableType, ComparableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? Float else { return false }
     return self == v
   }
+  @inlinable
   public func isSmaller(than object: Any?) -> Bool {
     guard let v = object as? Float else { return false }
     return self < v
   }
 }
 extension Double : EquatableType, ComparableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? Double else { return false }
     return self == v
   }
+  @inlinable
   public func isSmaller(than object: Any?) -> Bool {
     guard let v = object as? Double else { return false }
     return self < v
@@ -149,6 +185,7 @@ extension Double : EquatableType, ComparableType {
 }
 
 extension String : EquatableType, ComparableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let object = object else { return false } // other is nil
     switch object {
@@ -157,6 +194,7 @@ extension String : EquatableType, ComparableType {
       default: return false
     }
   }
+  @inlinable
   public func isSmaller(than object: Any?) -> Bool {
     guard let object = object else { return false } // other is nil
     switch object {
@@ -168,10 +206,12 @@ extension String : EquatableType, ComparableType {
 }
 
 extension Bool : EquatableType, ComparableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? Bool else { return false }
     return self == v
   }
+  @inlinable
   public func isSmaller(than object: Any?) -> Bool {
     guard let v = object as? Bool else { return false }
     return self == v ? false : !self
@@ -186,10 +226,12 @@ import struct Foundation.Date
 import struct Foundation.DateComponents
 
 extension Date : EquatableType, ComparableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? Date else { return false }
     return self == v
   }
+  @inlinable
   public func isSmaller(than object: Any?) -> Bool {
     guard let v = object as? Date else { return false }
     return self < v
@@ -197,24 +239,28 @@ extension Date : EquatableType, ComparableType {
 }
 
 extension UUID : EquatableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? UUID else { return false }
     return self == v
   }
 }
 extension URL : EquatableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? URL else { return false }
     return self == v
   }
 }
 extension Data : EquatableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? Data else { return false }
     return self == v
   }
 }
 extension DateComponents : EquatableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? DateComponents else { return false }
     return self == v
@@ -224,10 +270,12 @@ extension DateComponents : EquatableType {
 import struct Foundation.DateInterval
 @available(OSX 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *)
 extension DateInterval : EquatableType, ComparableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? DateInterval else { return false }
     return self == v
   }
+  @inlinable
   public func isSmaller(than object: Any?) -> Bool {
     guard let v = object as? DateInterval else { return false }
     return self < v
@@ -237,10 +285,12 @@ extension DateInterval : EquatableType, ComparableType {
 import struct Foundation.Measurement
 @available(OSX 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *)
 extension Measurement : EquatableType, ComparableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? Measurement else { return false }
     return self == v
   }
+  @inlinable
   public func isSmaller(than object: Any?) -> Bool {
     guard let v = object as? Measurement else { return false }
     return self < v
@@ -250,6 +300,7 @@ extension Measurement : EquatableType, ComparableType {
 import struct Foundation.PersonNameComponents
 @available(OSX 10.11, iOS 9.0, *)
 extension PersonNameComponents : EquatableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     guard let v = object as? PersonNameComponents else { return false }
     return self == v
@@ -257,6 +308,7 @@ extension PersonNameComponents : EquatableType {
 }
 
 extension Decimal : EquatableType, ComparableType {
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     switch object {
       case let other as Decimal: return self == other
@@ -273,6 +325,7 @@ extension Decimal : EquatableType, ComparableType {
       default: return false
     }
   }
+  @inlinable
   public func isSmaller(than object: Any?) -> Bool {
     guard let object = object else { return false }
     switch object {
@@ -299,6 +352,7 @@ extension Optional where Wrapped : EquatableType {
   // this is not picked
   // For this: you’ll need conditional conformance. Swift 4, hopefully
   
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     // TBD: do we want to compare optionals against non-optionals? In ObjC we
     //      do, right?
@@ -319,6 +373,7 @@ extension Optional where Wrapped : ComparableType {
   // this is not picked
   // For this: you’ll need conditional conformance. Swift 4, hopefully
   
+  @inlinable
   public func isSmaller(than object: Any?) -> Bool {
     // TBD: do we want to compare optionals against non-optionals? In ObjC we
     //      do, right?
@@ -340,6 +395,7 @@ extension Optional where Wrapped : ComparableType {
 
 extension Optional : EquatableType {
   
+  @inlinable
   public func isEqual(to object: Any?) -> Bool {
     // TBD: do we want to compare optionals against non-optionals? In ObjC we
     //      do, right?
@@ -365,6 +421,7 @@ extension Optional : EquatableType {
 }
 extension Optional : ComparableType {
   
+  @inlinable
   public func isSmaller(than object: Any?) -> Bool {
     // TBD: do we want to compare optionals against non-optionals? In ObjC we
     //      do, right?
@@ -394,6 +451,7 @@ extension Optional : ComparableType {
 // MARK: - Containment
 
 public extension Collection where Element : EquatableType {
+  @inlinable
   func contains(other object: Any?) -> Bool {
     return contains { $0.isEqual(to: object) }
   }
@@ -410,6 +468,7 @@ extension Range : ContainsComparisonType where Element : EquatableType {}
 #if swift(>=5)
 public extension StringProtocol {
 
+  @inlinable
   func contains(other object: Any?) -> Bool {
     switch object {
       case .none: return false
@@ -420,6 +479,7 @@ public extension StringProtocol {
     }
   }
   
+  @inlinable
   func isLike(other object: Any?, caseInsensitive ci: Bool) -> Bool {
     guard let other = object else { return false } // String not like nil
     if !ci {
@@ -442,6 +502,7 @@ public extension StringProtocol {
   
   /// A simple (and incomplete) * pattern matcher.
   /// Can only do prefix/suffix/contains
+  @inlinable
   func likePatternMatch<P: StringProtocol>(_ pattern: P) -> Bool {
     guard pattern.contains("*") else { return self == pattern }
     let starPrefix = pattern.hasPrefix("*")
