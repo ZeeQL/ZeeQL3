0e29cedfed8efe4db8f8c420b33f474281752f69
Helge Heß <devteam@zeezide.de>
2024-11-24 18:04:39 +0100

    Move createArgs extension to own file
    Used by more things...

--- Sources/ZeeQL/Access/AccessDataSource.swift
+++ Sources/ZeeQL/Access/AccessDataSource.swift
@@ -266,23 +266,3 @@ public extension AccessDataSourceType {
   }
 
 }
-
-fileprivate extension Dictionary where Key == String, Value == Any {
-  
-  /**
-   * This method creates a new `Dictionary` from a set of given array containing
-   * key/value arguments.
-   */
-  static func createArgs(_ values: [ Any ]) -> Self {
-    guard !values.isEmpty else { return [:] }
-    var me = Self()
-    me.reserveCapacity(values.count / 2 + 1)
-    
-    for idx in stride(from: 0, to: values.count, by: 2) {
-      let anyKey = values[idx]
-      let value  = values[idx + 1]
-      me[anyKey as? String ?? String(describing: anyKey)] = value
-    }
-    return me
-  }
-}
