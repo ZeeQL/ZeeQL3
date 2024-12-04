d8092542fb8fcab602ef53a95a6538b2d10fc5ed
Helge Heß <devteam@zeezide.de>
2024-12-04 18:06:05 +0100

    Override typed prefetch
    ... so that the spec stays typed.

--- Sources/ZeeQL/Control/FetchSpecification+Builder.swift
+++ Sources/ZeeQL/Control/FetchSpecification+Builder.swift
@@ -72,13 +72,25 @@ public extension DatabaseFetchSpecification {
   
   // MARK: - Prefetches
 
+  /// Note: This overrides previous prefetches!
   @inlinable
   func prefetch(_ path: String, _ more: String...) -> Self {
     var fs = self
     fs.prefetchingRelationshipKeyPathes = [ path ] + more
     return fs
   }
-  
+  /// Note: This overrides previous prefetches!
+  @inlinable
+  func prefetch(_ path: Relationship, _ more: Relationship...)
+       -> Self
+  {
+    // TODO: in here we cannot build pathes yet. Like:
+    //         `fs.prefetch(Person.e.company.addresses)`
+    var fs = self
+    fs.prefetchingRelationshipKeyPathes = [ path.name ] + more.map { $0.name }
+    return fs
+  }
+
   
   // MARK: - Ordering
   
@@ -117,5 +129,44 @@ public extension DatabaseFetchSpecification {
       fs.sortOrderings = ops
     }
     return fs
+  }
+}
+
+
+// MARK: - Special Builder for CodeEntities
+
+public extension DatabaseFetchSpecification
+  where Object: TypedEntityType, Object.FullEntity: CodeEntity<Object>
+{
+  
+  // TODO: select w/ pack iteration
+
+  @inlinable
+  func `where`<V>(_ key: Swift.KeyPath<Object.FullEntity, CodeAttribute<V>>,
+                  _ operation: ComparisonOperation,
+                  _ value: V) -> Self
+    where V: AttributeValue
+  {
+    let attribute = Object.e[keyPath: key]
+    return `where`(KeyValueQualifier(AttributeKey(attribute), operation, value))
+  }
+  
+  @inlinable
+  func `where`<V>(_ key: Swift.KeyPath<Object.FullEntity, CodeAttribute<V>>,
+                  _ value: V) -> Self
+    where V: AttributeValue
+  {
+    `where`(key, .EqualTo, value)
+  }
+
+  @inlinable
+  func order<V>(by key: Swift.KeyPath<Object.FullEntity, CodeAttribute<V>>,
+                _ selector: SortOrdering.Selector = .CompareAscending)
+         -> Self
+    where V: AttributeValue
+  {
+    let attribute = Object.e[keyPath: key]
+    let so = SortOrdering(key: AttributeKey(attribute), selector: selector)
+    return order(by: so)
   }
 }
