55d26bb07fc2b379226a4a6dc4a8a569a8ca1469
Helge Heß <devteam@zeezide.de>
2026-04-12 15:29:05 +0200

    Add Date and Data/Array<UInt8> representables
    ...

--- Sources/ZeeQL/Access/AdaptorQueryColumnRepresentable.swift
+++ Sources/ZeeQL/Access/AdaptorQueryColumnRepresentable.swift
@@ -6,11 +6,24 @@
 //  Copyright © 2017-2026 ZeeZide GmbH. All rights reserved.
 //
 
+#if canImport(Foundation)
+import struct Foundation.Data
+import struct Foundation.Date
+import struct Foundation.TimeInterval
+#endif
+#if canImport(Darwin)
+import Darwin
+#elseif canImport(Glibc)
+import Glibc
+#elseif canImport(Musl)
+import Musl
+#endif
+
 /**
  * A helper protocol that is used to convert ``AdaptorRecord`` columns into
  * Swift typed values.
  */
-public protocol AdaptorQueryColumnRepresentable {
+public protocol AdaptorQueryColumnRepresentable: Sendable {
 
   static func fromAdaptorQueryValue(_ value: Any?) throws -> Self
 }
@@ -65,17 +78,18 @@ extension BinaryInteger {
     
     switch value {
       case let typedValue as Int:    return Self(typedValue)
-      case let typedValue as Int64:  return Self(typedValue)
       case let typedValue as Int32:  return Self(typedValue)
+      case let typedValue as Int64:  return Self(typedValue)
       case let typedValue as String:
         guard let i = Int(typedValue) else {
-          throw AdaptorQueryTypeError.cannotConvertValue(Int.self, value)
+          throw AdaptorQueryTypeError.cannotConvertValue(Self.self, value)
          }
         return Self(i)
       default:
         // ERROR: VALUE: 9999 Int32
-        globalZeeQLLogger.error("VALUE: \(value) \(type(of: value))")
-        throw AdaptorQueryTypeError.cannotConvertValue(Int.self, value)
+        globalZeeQLLogger
+          .error("Cannot convert value to Int: \(value) \(type(of: value))")
+        throw AdaptorQueryTypeError.cannotConvertValue(Self.self, value)
     }
   }
 }
@@ -117,6 +131,104 @@ extension BinaryFloatingPoint {
   }
 }
 
+#if canImport(Foundation)
+extension Date: AdaptorQueryColumnRepresentable {
+
+  @inlinable
+  public static func fromAdaptorQueryValue(_ value: Any?) throws -> Date {
+    guard let value = value else {
+      throw AdaptorQueryTypeError.nullInNonOptionalType(Date.self)
+    }
+    if let date = value as? Date { return date }
+
+    if #available(macOS 15, iOS 13, *) {
+      if let v = value as? any BinaryInteger {
+        return Date(timeIntervalSince1970: TimeInterval(v))
+      }
+      if let v = value as? any BinaryFloatingPoint {
+        return Date(timeIntervalSince1970: TimeInterval(v))
+      }
+    }
+
+    // Fallback for older runtimes
+    if let v = value as? Int64 {
+      return Date(timeIntervalSince1970: TimeInterval(v))
+    }
+    if let v = value as? Double {
+      return Date(timeIntervalSince1970: v)
+    }
+
+    if let s = value as? String {
+      if let date = DateParser.parseISO8601(s) { return date }
+      throw AdaptorQueryTypeError
+        .cannotConvertValue(Date.self, value)
+    }
+
+    throw AdaptorQueryTypeError
+      .cannotConvertValue(Date.self, value)
+  }
+}
+
+/**
+ * Parses ISO 8601 date strings as commonly returned by SQLite and PostgreSQL
+ * text mode.
+ *
+ * Supported formats:
+ * - `"2024-05-08T12:00:00Z"`
+ * - `"2024-05-08 12:00:00"`
+ * - `"2024-05-08"`
+ */
+@usableFromInline
+internal enum DateParser {
+
+  private static let formats = [ "%Y-%m-%dT%H:%M:%SZ", "%Y-%m-%dT%H:%M:%S",
+                                 "%Y-%m-%d %H:%M:%S",  "%Y-%m-%d" ]
+
+  @usableFromInline
+  static func parseISO8601(_ string: String) -> Date? {
+    var tm = tm()
+    for fmt in formats {
+      memset(&tm, 0, MemoryLayout<tm>.size)
+      if strptime(string, fmt, &tm) != nil {
+        let time = timegm(&tm)
+        if time != -1 { return Date(timeIntervalSince1970: TimeInterval(time)) }
+      }
+    }
+    return nil
+  }
+}
+
+extension Data: AdaptorQueryColumnRepresentable {
+
+  @inlinable
+  public static func fromAdaptorQueryValue(_ value: Any?) throws -> Data {
+    guard let value = value else {
+      throw AdaptorQueryTypeError.nullInNonOptionalType(Self.self)
+    }
+    if let data  = value as? Self       { return data }
+    if let bytes = value as? [ UInt8 ]  { return Self(bytes) }
+    if let s     = value as? String     { return Self(s.utf8) }
+    throw AdaptorQueryTypeError.cannotConvertValue(Self.self, value)
+  }
+}
+#endif // canImport(Foundation)
+
+extension Array: AdaptorQueryColumnRepresentable where Element == UInt8 {
+
+  @inlinable
+  public static func fromAdaptorQueryValue(_ value: Any?) throws -> [ UInt8 ] {
+    guard let value = value else {
+      throw AdaptorQueryTypeError.nullInNonOptionalType([ UInt8 ].self)
+    }
+    if let bytes = value as? Self { return bytes }
+    #if canImport(Foundation)
+    if let data = value as? Data { return Self(data) }
+    #endif
+    if let s = value as? String { return Self(s.utf8) }
+    throw AdaptorQueryTypeError.cannotConvertValue([ UInt8 ].self, value)
+  }
+}
+
 extension Optional where Wrapped : AdaptorQueryColumnRepresentable {
   // this is not picked
   // For this: you’ll need conditional conformance. Swift 4, hopefully
@@ -139,10 +251,9 @@ extension Optional : AdaptorQueryColumnRepresentable {
   {
     guard let value = value else { return .none }
     
-    guard let c = Wrapped.self as? AdaptorQueryColumnRepresentable.Type
-     else {
+    guard let c = Wrapped.self as? AdaptorQueryColumnRepresentable.Type else {
       throw AdaptorQueryTypeError.cannotConvertValue(Int.self, value)
-     }
+    }
     
     return try c.fromAdaptorQueryValue(value) as? Wrapped
   }
