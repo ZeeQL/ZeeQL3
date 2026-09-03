81843770f7cabff2daa2ad101be0cf90153c9eb3
Helge Heß <devteam@zeezide.de>
2026-09-03 17:35:56 +0200

    Allow a few more datatypes in bindings
    Reject unsupported/invalid types.

--- Sources/ZeeQL/SQLite3Adaptor/SQLite3AdaptorChannel.swift
+++ Sources/ZeeQL/SQLite3Adaptor/SQLite3AdaptorChannel.swift
@@ -6,7 +6,16 @@
 //  Copyright © 2017-2026 ZeeZide GmbH. All rights reserved.
 //
 
+#if canImport(Foundation)
 import struct Foundation.Data
+import struct Foundation.Date
+import struct Foundation.Decimal
+import struct Foundation.Locale
+import struct Foundation.URL
+import struct Foundation.UUID
+import func Foundation.NSDecimalString
+#endif
+
 #if canImport(SQLite3)
   import SQLite3
 #elseif canImport(CSQLite3)
@@ -16,9 +25,11 @@ import struct Foundation.Data
 #if os(Linux)
   import func Glibc.strdup
   import func Glibc.free
+  import func Glibc.malloc
 #else
   import func Darwin.strdup
   import func Darwin.free
+  import func Darwin.malloc
 #endif
 
 public enum SQLite3AdaptorChannelError: Swift.Error {
@@ -27,6 +38,8 @@ public enum SQLite3AdaptorChannelError: Swift.Error {
   case rowFetchFailed  (Int32, String?)
   case bindFailed      (Int32, String?, SQLExpression.BindVariable)
 }
+
+private let decimalLocale = Locale(identifier: "en_US_POSIX")
 
 open class SQLite3AdaptorChannel : AdaptorChannel {
 
@@ -182,14 +195,24 @@ open class SQLite3AdaptorChannel : AdaptorChannel {
         return sqlite3_column_double(stmt, column)
       
       case SQLITE_BLOB:
+        let count = sqlite3_column_bytes(stmt, column)
+        #if canImport(Foundation)
+        if count == 0 { return Data() }
         if let blob = sqlite3_column_blob(stmt, column) {
-          let count = sqlite3_column_bytes(stmt, column)
-          let data  = Data(bytes: blob, count: Int(count))
-          return data
+          return Data(bytes: blob, count: Int(count))
         }
         else {
           return Optional<Data>.none
         }
+        #else
+        if count == 0 { return [ UInt8 ]() }
+        if let blob = sqlite3_column_blob(stmt, column) {
+          return [ UInt8 ](bytes: blob, count: Int(count))
+        }
+        else {
+          return Optional<[ UInt8 ]>.none
+        }
+        #endif
       
       default:
         if let cstr = sqlite3_column_text(stmt, column) {
@@ -329,13 +352,39 @@ open class SQLite3AdaptorChannel : AdaptorChannel {
                      pool    : Pool) throws
   {
     var idx : Int32 = 0
-    for bind in binds {
+    for variable in binds {
       idx += 1
-      // if doLogSQL { log.log("  BIND[\(idx)]: \(bind)") }
-      if let attr = bind.attribute {
+      // if doLogSQL { log.log("  BIND[\(idx)]: \(variable)") }
+      if let attr = variable.attribute {
         if doLogSQL { log.log("  BIND[\(idx)]: \(attr.name)") }
       }
-      
+
+      func bind(_ value: String) -> Int32 {
+        guard let text = pool.pstrdup(value) else { return SQLITE_NOMEM }
+        return sqlite3_bind_text(stmt, idx, text, -1, nil)
+      }
+
+      func bind<I: BinaryInteger>(_ value: I) -> Int32 {
+        guard let value = Int64(exactly: value) else { return SQLITE_RANGE }
+        return sqlite3_bind_int64(stmt, idx, sqlite3_int64(value))
+      }
+
+      #if canImport(Foundation)
+      func bind(_ value: Data) -> Int32 {
+        guard !value.isEmpty else { return sqlite3_bind_zeroblob(stmt, idx, 0) }
+        guard let count = Int32(exactly: value.count) else {
+          return SQLITE_TOOBIG
+        }
+        guard let bytes = pool.copy(value) else { return SQLITE_NOMEM }
+        return sqlite3_bind_blob(stmt, idx, bytes, count, nil)
+      }
+
+      func bind(_ value: Decimal) -> Int32 {
+        var value = value
+        return bind(NSDecimalString(&value, decimalLocale))
+      }
+      #endif
+
       func bindAnyValue(_ value: Any?) throws -> Int32 {
         guard let value = value else {
           if doLogBindValues { log.log("      [\(idx)]> bind NULL") }
@@ -346,19 +395,48 @@ open class SQLite3AdaptorChannel : AdaptorChannel {
             if doLogBindValues {
               log.log("      [\(idx)]> bind string \"\(value)\"")
             }
-            return sqlite3_bind_text(stmt, idx, pool.pstrdup(value), -1, nil)
+            return bind(value)
+          case let value as Bool:
+            if doLogBindValues { log.log("      [\(idx)]> bind bool \(value)") }
+            return bind(value ? 1 : 0)
           case let value as Int:
             if doLogBindValues { log.log("      [\(idx)]> bind int \(value)") }
-            return sqlite3_bind_int64(stmt, idx, sqlite3_int64(value))
+            return bind(value)
+          case let value as Int8:
+            if doLogBindValues { log.log("      [\(idx)]> bind int8 \(value)") }
+            return bind(value)
           case let value as Int32:
             if doLogBindValues { log.log("      [\(idx)]> bind int \(value)") }
-            return sqlite3_bind_int64(stmt, idx, sqlite3_int64(value))
+            return bind(value)
           case let value as Int64:
             if doLogBindValues { log.log("      [\(idx)]> bind int \(value)") }
-            return sqlite3_bind_int64(stmt, idx, sqlite3_int64(value))
+            return bind(value)
+          case let value as Float:
+            return sqlite3_bind_double(stmt, idx, Double(value))
+          case let value as Double:
+            return sqlite3_bind_double(stmt, idx, value)
+
+          #if canImport(Foundation)
+          case let value as Data:
+            if doLogBindValues { log.log("      [\(idx)]> bind Data \(value)") }
+            return bind(value)
+          case let value as Date:
+            if doLogBindValues { log.log("      [\(idx)]> bind Date \(value)") }
+            return sqlite3_bind_double(stmt, idx, value.timeIntervalSince1970)
+          case let value as Decimal:
+            if doLogBindValues { log.log("      [\(idx)]> bind Dec. \(value)") }
+            return bind(value)
+          case let value as UUID:
+            if doLogBindValues { log.log("      [\(idx)]> bind UUID \(value)") }
+            return bind(value.uuidString)
+          case let value as URL:
+            if doLogBindValues { log.log("      [\(idx)]> bind URL \(value)") }
+            return bind(value.absoluteString)
+          #endif
+
           case let value as GlobalID:
             #if !GLOBALID_AS_OPEN_CLASS
-            assert(value.keyCount == 1)
+            guard value.keyCount == 1 else { return SQLITE_MISMATCH }
             switch value.value {
               case .singleNil:
                 if doLogBindValues { log.log("      [\(idx)]> bind NULL") }
@@ -367,28 +445,24 @@ open class SQLite3AdaptorChannel : AdaptorChannel {
                 if doLogBindValues {
                   log.log("      [\(idx)]> bind int \(value)")
                 }
-                return sqlite3_bind_int64(stmt, idx, sqlite3_int64(value))
+                return bind(value)
               case .string(let value):
                 if doLogBindValues {
                   log.log("      [\(idx)]> bind string \"\(value)\"") }
-                return sqlite3_bind_text(stmt, idx, pool.pstrdup(value), -1, nil)
+                return bind(value)
               case .uuid(let value):
                 if doLogBindValues {
                   log.log("      [\(idx)]> bind string \"\(value)\"") }
-                return sqlite3_bind_text(stmt, idx,
-                                       pool.pstrdup(value.uuidString), -1, nil)
+                return bind(value.uuidString)
               case .values(let values):
                 if values.count > 1 {
-                  let rc = SQLITE_MISMATCH // TBD
-                  throw SQLite3AdaptorChannelError.bindFailed(rc, message(for: rc), bind)
+                  return SQLITE_MISMATCH
                 }
                 if let value = values.first {
                   return try bindAnyValue(value)
                 }
                 else {
-                  assertionFailure("Empty key")
-                  if doLogBindValues { log.log("      [\(idx)]> bind NULL") }
-                  return sqlite3_bind_null(stmt, idx)
+                  return SQLITE_MISMATCH
                 }
             }
             #else
@@ -396,25 +470,28 @@ open class SQLite3AdaptorChannel : AdaptorChannel {
                     value.keyCount == 1 else
               {
                 let rc = SQLITE_MISMATCH
-                throw SQLite3AdaptorChannelError.bindFailed(
-                  rc, message(for: rc), bind)
+                throw SQLite3AdaptorChannelError
+                        .bindFailed(rc, message(for: rc), variable)
               }
               return try bindAnyValue(value[0])
             #endif
+
+          case let value as any BinaryInteger:
+            if doLogBindValues { log.log("      [\(idx)]> bind int \(value)") }
+            return bind(value)
+
           default:
-            assertionFailure("Unexpected value, please add explicit type")
-            if doLogBindValues {
-              log.log("      [\(idx)]> bind other \(value)")
-            }
-            return sqlite3_bind_text(stmt, idx, pool.pstrdup(value), -1, nil)
+            return SQLITE_MISMATCH
         }
       }
       
       // TODO: Add a protocol to do this?
-      let rc = try bindAnyValue(bind.value)
+      let rc = try bindAnyValue(variable.value)
       
-      guard rc == SQLITE_OK
-       else { throw SQLite3AdaptorChannelError.bindFailed(rc, message(for: rc), bind) }
+      guard rc == SQLITE_OK else {
+        throw SQLite3AdaptorChannelError.bindFailed(rc, message(for: rc),
+                                                    variable)
+      }
     }
   }
   
@@ -478,13 +555,18 @@ open class SQLite3AdaptorChannel : AdaptorChannel {
     
     final var pointers = [ UnsafeMutableRawPointer ]()
     
-    func pstrdup(_ s: String) -> UnsafeMutablePointer<Int8> {
-      let p = strdup(s)! // assume it never fails ...
+    func pstrdup(_ s: String) -> UnsafeMutablePointer<Int8>? {
+      guard let p = strdup(s) else { return nil }
       pointers.append(p)
       return p
     }
-    func pstrdup(_ value: Any) -> UnsafeMutablePointer<Int8> {
-      return pstrdup(String(describing: value))
+
+    func copy(_ data: Data) -> UnsafeMutableRawPointer? {
+      guard let pointer = malloc(data.count) else { return nil }
+      data.copyBytes(to: pointer.assumingMemoryBound(to: UInt8.self),
+                     count: data.count)
+      pointers.append(pointer)
+      return pointer
     }
     
     deinit {
